7995057965df8b712c9d53ed8c936b1413ed052e
Breno do Val <brenorvale@gmail.com>
2022-07-04 23:39:18 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -181,23 +181,43 @@ describe('Signup Controller', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
-})
 
-test('Should call AddAccount with correct values', () => {
-  const { sut, addAccountStub } = makeSut()
-  const addSpy = jest.spyOn(addAccountStub, 'add')
-  const httpRequest = {
-    body: {
+  test('Should call AddAccount with correct values', () => {
+    const { sut, addAccountStub } = makeSut()
+    const addSpy = jest.spyOn(addAccountStub, 'add')
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(addSpy).toHaveBeenLastCalledWith({
       name: 'any_name',
       email: 'any_email@mail.com',
-      password: 'any_password',
-      passwordConfirmation: 'any_password'
+      password: 'any_password'
+    })
+  })
+
+  test('Should return 200 if valid data is provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
     }
-  }
-  sut.handle(httpRequest)
-  expect(addSpy).toHaveBeenLastCalledWith({
-    name: 'any_name',
-    email: 'any_email@mail.com',
-    password: 'any_password'
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    })
   })
 })
